9bf35e0e03bca42d62ca01ba8a7bed48f7f5c6bd
LuwinWickramasinghe <luwin.anuka@gmail.com>
2025-02-24 12:41:55 +0530

    routing modifications

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-places',
+  standalone: false,
   templateUrl: './places.page.html',
   styleUrls: ['./places.page.scss'],
 })
